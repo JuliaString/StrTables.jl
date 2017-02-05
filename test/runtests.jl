ae78e0183fcbdeba57b3e3481fe6a018b785a8d3
ScottPJones <scottjones@alum.mit.edu>
2017-02-05 22:39:29 +0100

    Update test of too large string to only run on 64-bit platforms

--- test/runtests.jl
+++ test/runtests.jl
@@ -53,7 +53,9 @@ testout = [stab, btab,
 @testset "Read/write values" begin
     io = IOBuffer(b"\x7f")
     @test_throws ErrorException ST.read_value(io)
-    @test_throws ErrorException ST.write_value(io, String(Vector{UInt8}(2^32)))
+    @static if sizeof(Int) > 4
+        @test_throws ErrorException ST.write_value(io, String(Vector{UInt8}(2^32)))
+    end
 end
 
 @testset "Save/Load tables" begin
